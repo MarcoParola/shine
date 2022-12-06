89add6eb06ef38395253bd119e1b4dc44f68714f
MarcoParola <marcoparola96@gmail.com>
2022-12-06 16:21:03 +0100

    clean main file

--- main.py
+++ main.py
@@ -8,30 +8,6 @@ from src.datasets import ImageDataset
 from src.algorithms import HierarchicalSlider
 
 
-
-class SlidingAnalyzer():
-
-    def __init__(self):
-        print("we")
-
-    def analyze_dataset(self, dataset):
-        for img in dataset:
-            print(img.shape)
-            self.analyze_image(img)
-
-    def analyze_image(self, img):
-        image_width = len(img)
-        image_height = len(img[0])
-        min_dim = min(image_width, image_height)
-        print(image_height)
-
-        for grid in range(MIN_GRID_BLOCK, MAX_GRID_BLOCK):
-            analyze_image_by_grid(img, grid)
-
-
-
-
-
 @hydra.main(config_path="config", config_name="config")
 def main(cfg):
     
@@ -39,11 +15,6 @@ def main(cfg):
     hierch_slider = HierarchicalSlider(cfg)
     hierch_slider.run(dataset)
     
-    
-    
-
-    
-
     
 if __name__ == "__main__":
     main()
